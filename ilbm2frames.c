8f6d38dc8213f3a171984e293bb884945127dcac
Robert Schultz <robert@cosmicrealms.com>
2022-01-20 09:44:52 -0500

    Updated to output in JSON various color cycling info to help determine if the output frames are valid or not

--- ilbm2frames.c
+++ ilbm2frames.c
@@ -102,7 +102,7 @@ static void parse_options(int argc, char **argv)
 	outputDirPath = argv[1];
 }
 
-uint32_t ilbm2frames(SDL_ILBM_Set * set, unsigned int num)
+void ilbm2frames(json_t * imageJSON, SDL_ILBM_Set * set, unsigned int num)
 {
 	struct timeval tv;
 	uint32_t msPerFrame = 1000/fps;
@@ -114,14 +114,57 @@ uint32_t ilbm2frames(SDL_ILBM_Set * set, unsigned int num)
 	if(image==NULL)
 	{
 	    fprintf(stderr, "Cannot open image #%d\n", num);
-		return 0;
+		return;
 	}
 
-	if(image->image->colorRangeLength==0 && image->image->cycleInfoLength==0)
+	if(image->image->colorRangeLength==0 && image->image->cycleInfoLength==0 && image->image->drangeLength==0)
 	{
 		SDL_ILBM_freeImage(image);
-		return 0;
+		return;
 	}
+
+	json_t * colorRanges=json_array();
+	for(uint32_t i=0;i<image->image->colorRangeLength;i++)
+	{
+		json_t * colorRange = json_object();
+		json_object_set(colorRange, "pad1", json_integer(image->image->colorRange[i]->pad1));
+		json_object_set(colorRange, "rate", json_integer(image->image->colorRange[i]->rate));
+		json_object_set(colorRange, "active", json_integer(image->image->colorRange[i]->active));
+		json_object_set(colorRange, "low", json_integer(image->image->colorRange[i]->low));
+		json_object_set(colorRange, "high", json_integer(image->image->colorRange[i]->high));
+		json_array_append(colorRanges, colorRange);
+	}
+	json_object_set(imageJSON, "colorRanges", colorRanges);
+
+	json_t * cycleInfos=json_array();
+	for(uint32_t i=0;i<image->image->cycleInfoLength;i++)
+	{
+		json_t * cycleInfo = json_object();
+		json_object_set(cycleInfo, "pad", json_integer(image->image->cycleInfo[i]->pad));
+		json_object_set(cycleInfo, "pad1", json_integer(image->image->cycleInfo[i]->direction));
+		json_object_set(cycleInfo, "start", json_integer(image->image->cycleInfo[i]->start));
+		json_object_set(cycleInfo, "end", json_integer(image->image->cycleInfo[i]->end));
+		json_object_set(cycleInfo, "seconds", json_integer(image->image->cycleInfo[i]->seconds));
+		json_object_set(cycleInfo, "microSeconds", json_integer(image->image->cycleInfo[i]->microSeconds));
+		json_array_append(cycleInfos, cycleInfo);
+	}
+	json_object_set(imageJSON, "cycleInfos", cycleInfos);
+
+	json_t * dRanges=json_array();
+	for(uint32_t i=0;i<image->image->drangeLength;i++)
+	{
+		json_t * dRange = json_object();
+		json_object_set(dRange, "min", json_integer(image->image->drange[i]->min));
+		json_object_set(dRange, "max", json_integer(image->image->drange[i]->max));
+		json_object_set(dRange, "rate", json_integer(image->image->drange[i]->rate));
+		json_object_set(dRange, "flags", json_integer(image->image->drange[i]->flags));
+		json_object_set(dRange, "ntrue", json_integer(image->image->drange[i]->ntrue));
+		json_object_set(dRange, "nregs", json_integer(image->image->drange[i]->nregs));
+		json_object_set(dRange, "nfades", json_integer(image->image->drange[i]->nfades));
+		json_object_set(dRange, "pad", json_integer(image->image->drange[i]->pad));
+		json_array_append(dRanges, dRange);
+	}
+	json_object_set(imageJSON, "dRanges", dRanges);
 
 	for(uint32_t i=0;i<maxFrames;i++)
 		surfaces[i] = SDL_ILBM_createSurface(image->image, 0, SDL_ILBM_AUTO_FORMAT);
@@ -161,7 +204,8 @@ uint32_t ilbm2frames(SDL_ILBM_Set * set, unsigned int num)
 	free(surfaces);
 
 	SDL_ILBM_freeImage(image);
-	return frameNum;
+
+	json_object_set(imageJSON, "frameCount", json_integer(frameNum));
 }
 
 int main(int argc, char ** argv)
@@ -190,14 +234,14 @@ int main(int argc, char ** argv)
 	for(unsigned int num=0;num<set->imagesLength;num++)
 	{
 		json_t * imageJSON = json_object();
-		uint32_t frameCount = ilbm2frames(set, num);
-		json_object_set(imageJSON, "frameCount", json_integer(frameCount));
+		ilbm2frames(imageJSON, set, num);
 		json_array_append(imagesJSON, imageJSON);
 	}
 	json_object_set(json, "images", imagesJSON);
 
+	printf("%s\n", json_dumps(json, 0));
+	fflush(stdout);
+
     SDL_ILBM_freeSet(set);
 	SDL_Quit();
-
-	printf("%s\n", json_dumps(json, 0));
 }
